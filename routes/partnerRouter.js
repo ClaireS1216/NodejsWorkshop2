e3c3525e0193fdad2c7da8147680783bcda55399
ClaireShih <ginobili20g@Claires-MacBook-Pro.local>
2021-04-11 21:07:44 -0700

    Express Generator REST API

---
+++ routes/partnerRouter.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const partnerRouter = express.Router();
+
+partnerRouter
+  .route("/")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res) => {
+    res.end("Will send all the partners to you");
+  })
+  .post((req, res) => {
+    res.end(
+      `Will add the partner: ${req.body.name} with description: ${req.body.description}`
+    );
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /partners");
+  })
+  .delete((req, res) => {
+    res.end("Deleting all parnters");
+  });
+
+partnerRouter
+  .route("/:partnerId")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res) => {
+    res.end(`Will send details of the parnter: ${req.params.parnterId} to you`);
+  })
+  .post((req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `POST operation not supported on /partners/${req.params.parnterId}`
+    );
+  })
+
+  .put((req, res) => {
+    res.write(`Updating the partner: ${req.params.partnerId}\n`);
+    res.end(`Will update the partner: ${req.body.name}
+        with description: ${req.body.description}`);
+  })
+  .delete((req, res) => {
+    res.end(`Deleting partners: ${req.params.partnerId}`);
+  });
+
+module.exports = partnerRouter;
